c4cd92adf0e40ce8fa8b2d3c4ad95ec161aa116c
Phatcharaphol Suanprom <psuanpro@z3t4c1.42bangkok.com>
2023-01-14 23:46:50 +0700

    parser init

--- src/lexer/lexer_split.c
+++ src/lexer/lexer_split.c
@@ -6,7 +6,7 @@
 /*   By: psuanpro <Marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 19:34:37 by psuanpro          #+#    #+#             */
-/*   Updated: 2023/01/07 21:47:57 by psuanpro         ###   ########.fr       */
+/*   Updated: 2023/01/14 23:02:33 by psuanpro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,4 @@
 
 
 
-// int	main(void)
-// {
-// 	char *s = "$(hello)";
-// 	char **ret;
 
-// 	ret = lexer_split(s);
-
-// 	for (int i = 0; ret[i]; i++)
-// 		printf("ret[i] -> %s\n", ret[i]);
-	
-// 	printf("%s--------------------%s\n", "\e[42m", "\e[0m");
-// 	s = "$HELLO";
-// 	ret = lexer_split(s);
-
-// 	for (int i = 0; ret[i]; i++)
-// 		printf("ret[i] -> %s\n", ret[i]);
-// 	printf("%s--------------------%s\n", "\e[42m", "\e[0m");
-// 	s = "${HELLO}";
-// 	ret = lexer_split(s);
-
-// 	for (int i = 0; ret[i]; i++)
-// 		printf("ret[i] -> %s\n", ret[i]);
-// 	return (0);
-// }
-// char	*s = "touch ABC|echo \" \'hello\' \" > ABC | echo he\'ll\'o >> ABC | cat ABC |ls| echo \" hel|lo \"";
--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: psuanpro <Marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 22:52:55 by psuanpro          #+#    #+#             */
-/*   Updated: 2023/01/14 22:30:25 by psuanpro         ###   ########.fr       */
+/*   Updated: 2023/01/14 23:44:36 by psuanpro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,48 @@ int	len_pipe(t_llst *p)
 	return (len + 1);
 }
 
+int	len_cmd_parser(t_llst *p)
+{
+	int	len;
+
+	while (p != NULL && ft_strncmp(p->content, "|", 2))
+	{
+		len++;
+		p = p->next;
+	}
+	return (len);
+}
+
+char	**get_allcmd(t_llst *lst, int size)
+{
+	char	**ret;
+	int		i;
+
+	i = 0;
+	ret = (char **)malloc(sizeof(char *) * (size + 1));
+	if (!ret)
+		return (NULL);
+	while (lst && i < size)
+	{
+		ret[i] = ft_strdup(lst->content);
+		i++;
+	}
+	ret[i] = NULL;
+	return (ret);
+}
+
+t_cmd	init_cmd_parser(t_llst *p, int idx)
+{
+	t_cmd	new;
+	int		len;
+
+	len = len_cmd_parser(p);
+	new.index = idx;
+	new.allcmd = get_allcmd(p, len);
+
+	return (new);
+}
+
 void	create_cmd_parser(t_pro *p, t_llst *lst)
 {
 	int	i;
@@ -44,14 +86,13 @@ void	create_cmd_parser(t_pro *p, t_llst *lst)
 	p->par.cmd = (t_cmd *)malloc(sizeof(t_cmd) * p->par.size);
 	if (!p->par.cmd)
 		return ;
-	while (i < p->par.size)
+	while (lst && i < p->par.size)
 	{
-		while (lst != NULL)
-		{
-			printf("lst->content -> %s\n", lst->content);
-			lst = lst->next;
-		}
-		i++;
+		if (!ft_strncmp(lst->content, "|", 2))
+			i++;
+
+		p->par.cmd[i] = init_cmd_parser(lst, i);
+		lst = lst->next;
 	}
 }
 
