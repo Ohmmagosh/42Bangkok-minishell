6ee27bc0a1b04b90beaec7a7bf8433911e02ff31
Phatcharaphol Suanprom <psuanpro@z3t4c1.42bangkok.com>
2023-01-23 23:35:59 +0700

    clear redirect before execute

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: psuanpro <Marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 22:52:55 by psuanpro          #+#    #+#             */
-/*   Updated: 2023/01/23 02:20:06 by psuanpro         ###   ########.fr       */
+/*   Updated: 2023/01/23 23:35:23 by psuanpro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -461,41 +461,123 @@ void	chk_redirect(t_pro *p)
 	}
 }
 
-void	new_str(char *re, char *file)
-{
-	
-}
+// char	**new_allcmd(char **cmd)
+// {
+// 	int	i;
 
-char	**new_allcmd(char **cmd)
-{
-	int	i;
-
-	i = 0;
-	while (cmd[i] && cmd[i + 1])
-	{
-		if (!ft_strncmp(cmd[i], "<", 2))
-			new_str(cmd[i], cmd[i + 1]);
-		else if (!ft_strncmp(cmd[i], "<<", 3))
-			continue;
-		else if (!ft_strncmp(cmd[i], ">", 2))
-			continue;
-		else if (!ft_strncmp(cmd[i], ">>", 3))
-			continue;
-		i++;
-	}
-}
+// 	i = 0;
+// 	while (cmd[i] && cmd[i + 1])
+// 	{
+// 		if (!ft_strncmp(cmd[i], "<", 2))
+// 			new_str(cmd[i], cmd[i + 1]);
+// 		else if (!ft_strncmp(cmd[i], "<<", 3))
+// 			continue;
+// 		else if (!ft_strncmp(cmd[i], ">", 2))
+// 			continue;
+// 		else if (!ft_strncmp(cmd[i], ">>", 3))
+// 			continue;
+// 		i++;
+// 	}
+// }
 
 // void	print_chk_cmd2()
-void	clear_redirect(t_pro *p)
+// void	clear_redirect(t_pro *p)
+// {
+// 	int	i;
+
+// 	i = 0;
+// 	while (i < p->par.size)
+// 	{
+// 		p->par.cmd[i].allcmd = new_allcmd(p->par.cmd[i].allcmd);
+// 		i++;
+// 	}
+// }
+
+char	*newrealloc(char *s)
+{
+	free(s);
+	s = ft_calloc(1, 1);
+	return (s);
+}
+
+void	free_split(char **bye)
 {
 	int	i;
 
 	i = 0;
-	while (i < p->par.size)
+	if (!bye)
+		return;
+	while (bye[i])
 	{
-		p->par.cmd[i].allcmd = new_allcmd(p->par.cmd[i].allcmd);
+		free(bye[i]);
 		i++;
 	}
+	free(bye);
+}
+int	new_str_len(char **str)
+{
+	int	i;
+	int	len;
+
+	i = 0;
+	len = 0;
+	while (str[i])
+	{
+		if(ft_strncmp(str[i], "", 1))
+			len++;
+		i++;
+	}
+	return (len);
+}
+
+char	**new_str_utils(char **str)
+{
+	char	**ret;
+	int		len;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	len = new_str_len(str);
+	ret = (char **)malloc(sizeof(char *) * (len + 1));
+	if (!ret)
+		return (NULL);
+	i = 0;
+	while (str[i])
+	{
+		if(ft_strncmp(str[i], "", 1))
+		{
+			ret[j] = ft_strdup(str[i]);
+			j++;
+		}
+		i++;
+	}
+	ret[j] = NULL;
+	return (ret);
+}
+
+char	**new_str(char **cmd)
+{
+	int	i;
+	char	**ret;
+
+	i = 0;
+	ret = NULL;
+	while (cmd[i])
+	{
+		if (ismetastr(cmd[i]) && cmd[i])
+		{	
+			cmd[i] = newrealloc(cmd[i]);
+			if (cmd[i + 1])
+				cmd[i + 1] = newrealloc(cmd[i + 1]);
+		}
+		i++;
+	}
+	i = 0;
+	ret = new_str_utils(cmd);
+	
+	return (ret);
 }
 
 void	parser(t_pro *p)
--- test.c
+++ test.c
@@ -3,52 +3,119 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void	new_str(char *s1, char *s2)
+int	ismetastr(char *s)
 {
-	// char	*tmp;
-	// char	*tmp2;
+	if (s == NULL)
+		return (0);
+	if (!ft_strncmp(s, "<", 2))
+		return (1);
+	else if (!ft_strncmp(s, "<<", 3))
+		return (1);
+	else if (!ft_strncmp(s, ">", 2))
+		return (1);
+	else if (!ft_strncmp(s, ">>", 3))
+		return (1);
+	return (0);
+}
 
-	s1 = ft_strdup("-----");
-	s2 = ft_strdup("-----");
-	// s1 = tmp;
-	// s2 = tmp2;
+char	*newrealloc(char *s)
+{
+	free(s);
+	s = ft_calloc(1, 1);
+	return (s);
+}
+
+void	free_split(char **bye)
+{
+	int	i;
+
+	i = 0;
+	if (!bye)
+		return;
+	while (bye[i])
+	{
+		free(bye[i]);
+		i++;
+	}
+	free(bye);
+}
+int	new_str_len(char **str)
+{
+	int	i;
+	int	len;
+
+	i = 0;
+	len = 0;
+	while (str[i])
+	{
+		if(ft_strncmp(str[i], "", 1))
+			len++;
+		i++;
+	}
+	return (len);
+}
+
+char	**new_str_utils(char **str)
+{
+	char	**ret;
+	int		len;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	len = new_str_len(str);
+	ret = (char **)malloc(sizeof(char *) * (len + 1));
+	if (!ret)
+		return (NULL);
+	i = 0;
+	while (str[i])
+	{
+		if(ft_strncmp(str[i], "", 1))
+		{
+			ret[j] = ft_strdup(str[i]);
+			j++;
+		}
+		i++;
+	}
+	ret[j] = NULL;
+	return (ret);
+}
+
+char	**new_str(char **cmd)
+{
+	int	i;
+	char	**ret;
+
+	i = 0;
+	ret = NULL;
+	while (cmd[i])
+	{
+		if (ismetastr(cmd[i]) && cmd[i])
+		{	
+			cmd[i] = newrealloc(cmd[i]);
+			if (cmd[i + 1])
+				cmd[i + 1] = newrealloc(cmd[i + 1]);
+		}
+		i++;
+	}
+	i = 0;
+	ret = new_str_utils(cmd);
+	
+	return (ret);
 }
 
 int	main(void)
 {
-	// char **s =ft_split("hello world hiiiii", ' ');
-	// for (int i = 0; s[i]; i++)
-	// 	printf("s[i] -> %s\n", s[i]);
-	// // new_str(s[1], s[3]);
-	// printf("%s----------berfore new str----------%s\n", "\e[42m", "\e[0m");
-	// for (int i = 0; s[i]; i++)
-	// {
-	// 	if (!ft_strncmp(s[i], "world", 6))
-	// 	{
-	// 		free(s[i]);
-	// 		s[i] = ft_strdup("-----");
-	// 		if (s[i + 1])
-	// 		{
-	// 			free(s[i + 1]);
-	// 			s[i + 1] = ft_strdup("-----");
-	// 		}	
-	// 	}
-	// }
-	// for (int i = 0; s[i]; i++)
-	// 	printf("s[i] -> %s\n", s[i]);
-	// for (int i = 0; s[i]; i++)
-	// 	free(s[i]);
-	// free(s);
-	char	*s[] = {"hello", NULL, "world", NULL};
-	
-	for (int i = 0; i < 3; i++)
-		printf("%s\n", s[i]);
-	
-	for (int i = 0; i < 3; i++)
-	{
-		if (s[i] != NULL)
-			free(s[i]);
-	}
-	free(s);
-	
+	char	**s = ft_split("echo hello < hello < world > o1 > o2 << eof > o4", ' ');
+
+	char	**ret;
+
+	for (int i = 0; s[i]; i++)
+		printf("s[i] -> %s\n", s[i]);
+	ret = new_str(s);
+
+	for (int i = 0; ret[i]; i++)
+		printf("cmd[i] -> %s\n", ret[i]);
+
 }
